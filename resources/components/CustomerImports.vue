1812accfcdd7e8222d2d3db9c0335b3c48921ed8
Jay Aries Flores <aries@esixtycorporation.com>
2021-12-19 15:39:21 +0800

    add table responsive

--- resources/components/CustomerImports.vue
+++ resources/components/CustomerImports.vue
@@ -1,62 +1,64 @@
 <template>
     <div class="customer-imports">
-        <table class="table">
-            <thead>
-            <tr>
-                <th>File</th>
-                <th>Status</th>
-                <th>Total</th>
-                <th>Located</th>
-                <th>Download CSV</th>
-            </tr>
-            </thead>
-            <tbody>
-            <template v-if="customerImports.length > 0">
-                <tr v-for="(row,index) in customerImports" :key="index">
-                    <td>
-                        <a :href="`/imports/${row.id}/customers`">
-                            {{ row.file }}
-                        </a>
-                    </td>
-                    <td>{{ row.status }}</td>
-                    <td>
-                        <animated-number :value="row.total" :tween-duration="1500" />
-                    </td>
-                    <td>
-                        <template v-if="row.status === 'coordinate-searching'">
-                            <animated-number :value="row.success_count || 0" :tween-duration="1500" /> / <animated-number :value="row.search_elapse_count" :tween-duration="1500" />
-                        </template>
-                        <template v-else-if="row.success_count > 0">
-                            <animated-number :value="row.success_count || 0" :tween-duration="1500" />
-                            <button class="btn btn-outline-info btn-sm" @click="locateImportCustomers(row)">
-                                <i class="fa-solid fa-arrows-rotate"></i>
-                            </button>
-                        </template>
-                        <template v-else>
-                            <button class="btn btn-outline-info btn-sm" @click="locateImportCustomers(row)">
-                                Locate <i class="fa-solid fa-magnifying-glass-location ms-1"></i>
-                            </button>
-                        </template>
-                    </td>
-                    <td>
-                        <div class="btn-group">
-                            <button class="btn btn-sm btn-info" :disabled="!['coordinate-located','completed'].includes(row.status)" @click="generateCsvTrigger(row)">Generate</button>
-                            <a v-if="!!row.csv_path" :href="`/api/imports/${row.id}/download-csv`" :class="{'disabled' : row.status !== 'completed'}" class="btn btn-sm btn-primary">Download</a>
-                        </div>
-                    </td>
-                </tr>
-            </template>
-            <template v-else>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
                 <tr>
-                    <td colspan="99">
-                        <div class="alert alert-info text-center">
-                            No import yet.
-                        </div>
-                    </td>
+                    <th>File</th>
+                    <th>Status</th>
+                    <th>Total</th>
+                    <th>Located</th>
+                    <th>Download CSV</th>
                 </tr>
-            </template>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                <template v-if="customerImports.length > 0">
+                    <tr v-for="(row,index) in customerImports" :key="index">
+                        <td>
+                            <a :href="`/imports/${row.id}/customers`">
+                                {{ row.file }}
+                            </a>
+                        </td>
+                        <td>{{ row.status }}</td>
+                        <td>
+                            <animated-number :value="row.total" :tween-duration="1500" />
+                        </td>
+                        <td>
+                            <template v-if="row.status === 'coordinate-searching'">
+                                <animated-number :value="row.success_count || 0" :tween-duration="1500" /> / <animated-number :value="row.search_elapse_count" :tween-duration="1500" />
+                            </template>
+                            <template v-else-if="row.success_count > 0">
+                                <animated-number :value="row.success_count || 0" :tween-duration="1500" />
+                                <button class="btn btn-outline-info btn-sm ms-1" @click="locateImportCustomers(row)">
+                                    <i class="fa-solid fa-arrows-rotate"></i>
+                                </button>
+                            </template>
+                            <template v-else>
+                                <button class="btn btn-outline-info btn-sm" @click="locateImportCustomers(row)">
+                                    Locate <i class="fa-solid fa-magnifying-glass-location ms-1"></i>
+                                </button>
+                            </template>
+                        </td>
+                        <td>
+                            <div class="btn-group">
+                                <button class="btn btn-sm btn-info" :disabled="!['coordinate-located','completed'].includes(row.status)" @click="generateCsvTrigger(row)">Generate</button>
+                                <a v-if="!!row.csv_path" :href="`/api/imports/${row.id}/download-csv`" :class="{'disabled' : row.status !== 'completed'}" class="btn btn-sm btn-primary">Download</a>
+                            </div>
+                        </td>
+                    </tr>
+                </template>
+                <template v-else>
+                    <tr>
+                        <td colspan="99">
+                            <div class="alert alert-info text-center">
+                                No import yet.
+                            </div>
+                        </td>
+                    </tr>
+                </template>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
